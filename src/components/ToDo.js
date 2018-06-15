92bcef4fb8f15edb027c8557154a1098675ad7f0
jordan.crutcher <jordan.crutcher@gmail.com>
2018-06-15 16:00:25 -0500

    ToDo Components

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ToDo extends Components {
+  render() {
+    return(
+      <li> A task will go here </li>
+    );
+  }
+}
+
+export default ToDo;
